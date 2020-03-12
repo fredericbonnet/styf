773ae0a81e733ca000b6f580906b319f45e19a16
Frédéric BONNET <f.bonnet@groupe-creative.fr>
2020-03-12 12:20:06 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Stop Touching Your Face!
+
+Compulsive face touching is a major factor of virus propagation.
+This app displays a notification anytime it detects hand movements
+around your face. Help stop the propagation of COVID-9!
+
+## Usage
+
+1. Open the app page: https://styf.netlify.com
+2. Wait for model to load
+3. Click on the "Enable hand detection" switch
+4. Accept all permissions (webcam access and desktop notifications)
+5. Test the app by enabling video preview and moving your hand at head level
+6. Leave the tab open in the background as long as necessary, don't minimize the window! Disable video preview for better performances
+
+## Test locally
+
+To launch the app locally, clone this repository then run these commands:
+
+```
+npm ci
+npm start
+```
+
+This should start a HTTP server at http://127.0.0.1:8080.
+
+## Known issues
+
+The hand detection package sometimes detects false positive when moving your head. This is a minor disturbance.
+
+Don't pay attention to the quality of the code, it's a very quick-and-dirty app I wrote in a couple of hours.
+
+## Acknowledgements
+
+This app was made possible by the awesome
+[Handtrack.js](https://github.com/victordibia/handtrack.js/) package!
+
+The app page is based on Material Design Lite and more specifically the [Stand-alone article](https://getmdl.io/templates/article/index.html) template.
---
+++ www/js/main.js
@@ -0,0 +1,107 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const enableHandDetection = document.getElementById("enable-hand-detection");
+const statusLabel = document.getElementById("status");
+const videoPreview = document.getElementById("video-preview");
+const enableVideoPreview = document.getElementById("enable-video-preview");
+
+let isVideo = false;
+let isVideoPreview = false;
+let model = null;
+
+// video.width = 500
+// video.height = 400
+
+const modelParams = {
+  flipHorizontal: true, // flip e.g for video
+  maxNumBoxes: 20, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.7 // confidence threshold for predictions.
+};
+
+function startVideo() {
+  askForPermissions();
+  handTrack.startVideo(video).then(function(status) {
+    console.log("video started", status);
+    if (status) {
+      statusLabel.innerText = "Video started. Now tracking";
+      isVideo = true;
+      runDetection();
+    } else {
+      statusLabel.innerText = "Please enable video capture";
+    }
+  });
+}
+
+function toggleVideo() {
+  if (!isVideo) {
+    statusLabel.innerText = "Starting video...";
+    startVideo();
+  } else {
+    statusLabel.innerText = "Stopping video";
+    handTrack.stopVideo(video);
+    isVideo = false;
+    statusLabel.innerText = "Video stopped";
+  }
+}
+
+function toggleVideoPreview() {
+  isVideoPreview = !isVideoPreview;
+
+  videoPreview.style.display = isVideoPreview ? "" : "none";
+}
+
+enableHandDetection.addEventListener("change", function() {
+  toggleVideo();
+});
+
+enableVideoPreview.addEventListener("change", function() {
+  toggleVideoPreview();
+});
+
+function runDetection() {
+  model.detect(video).then(predictions => {
+    console.log("Predictions: ", predictions);
+    detectHand(video, predictions);
+    model.renderPredictions(predictions, canvas, context, video);
+    if (isVideo) {
+      requestAnimationFrame(runDetection);
+    }
+  });
+}
+
+let canNotify = false;
+function askForPermissions() {
+  Notification.requestPermission(status => {
+    canNotify = status === "granted";
+  });
+}
+
+function detectHand(video, predictions) {
+  const lowerThreshold = video.height * 0.7;
+  for (let prediction of predictions) {
+    const {
+      bbox: [, y, height]
+    } = prediction;
+    if (y + height / 2 > lowerThreshold) {
+      //   console.error("Stop Touching Your Face!");
+      if (canNotify) {
+        const notification = new Notification("Stop Touching Your Face!", {
+          body: "Fight the COVID-9 !",
+          tag: "styf"
+        });
+        setTimeout(() => notification.close(), 1000);
+        break;
+      }
+    }
+  }
+}
+
+// Load the model.
+statusLabel.innerText = "Loading model...";
+handTrack.load(modelParams).then(lmodel => {
+  model = lmodel;
+  statusLabel.innerText = "Model loaded!";
+  enableHandDetection.parentNode.MaterialSwitch.enable();
+});
